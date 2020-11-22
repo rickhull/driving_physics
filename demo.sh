00d12ef579fd0e02985942f16d2279a54c3a176a
Rick Hull <rwh@localhost>
2020-11-22 00:51:20 -0500

    add some helpers to workaround lacking rake

---
+++ demo.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+ruby -Ilib demo/driving_physics.rb
+ruby -Ilib demo/vector.rb
+ruby -Ilib demo/tire.rb
---
+++ test.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+ruby -Ilib test/driving_physics.rb
+ruby -Ilib test/vector.rb
+ruby -Ilib test/tire.rb
