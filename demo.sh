cab3bb0430f3fde90a12e190a96c8de291266108
Rick Hull <rwh@localhost>
2020-12-08 13:16:13 -0500

    remove demo.sh and test.sh in favor of Rakefile

--- demo.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-ruby -Ilib demo/scalar_force.rb
-ruby -Ilib demo/vector_force.rb
-ruby -Ilib demo/wheel.rb
-ruby -Ilib demo/car.rb
-ruby -Ilib demo/tire.rb
--- test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-ruby -Ilib test/driving_physics.rb
-ruby -Ilib test/scalar_force.rb
-ruby -Ilib test/vector_force.rb
-ruby -Ilib test/tire.rb
-ruby -Ilib test/wheel.rb
-ruby -Ilib test/car.rb
