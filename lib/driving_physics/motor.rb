0dbe9cacdd83a1c2627ab3b8e51070a442ee7970
Rick Hull <rick.hull@gmail.com>
2022-06-16 14:31:34 -0400

    add 330 peak at 5000 RPM

--- lib/driving_physics/motor.rb
+++ lib/driving_physics/motor.rb
@@ -6,7 +6,7 @@ module DrivingPhysics
     class OverRev < RuntimeError; end
     class SanityCheck < RuntimeError; end
 
-    TORQUES = [  0,   50,  130,  200,  250,  320,  320,  320,  260,    0]
+    TORQUES = [  0,   50,  130,  200,  250,  320,  330,  320,  260,    0]
     RPMS    = [500, 1000, 1500, 2000, 2500, 3500, 5000, 6000, 7000, 7100]
 
     attr_reader :env
