707b1702569d149646a6d7bfdb27b25f59ac2c95
Rick Hull <rick.hull@gmail.com>
2022-06-16 19:59:11 -0400

    rework gear ratios

--- lib/driving_physics/gearbox.rb
+++ lib/driving_physics/gearbox.rb
@@ -1,11 +1,21 @@
 require 'driving_physics/disk'
 
 module DrivingPhysics
+  # Technically speaking, the gear ratio goes up with speed and down with
+  # torque.  But in the automotive world, it's customary to invert the
+  # relationship, where a bigger number means more torque and less speed.
+  # We'll store the truth, where the default final drive would be
+  # conventionally known as 3.73, but 1/3.73 will be stored.
+
+  # The gear ratio (above 1) multiplies speed and divides torque
+  # 5000 RPM is around 375 MPH with a standard wheel/tire
+  # 3.73 final drive reduces that to around 100 mph in e.g. 5th gear (1.0)
+  # Likewise, 1st gear is a _smaller_ gear ratio than 3rd
   class Gearbox
     class Disengaged < RuntimeError; end
 
-    RATIOS = [5r, 5/2r, 9/5r, 7/5r, 1r, 4/5r]
-    REAR_END = 41/11r # 3.73
+    RATIOS = [1/5r, 2/5r, 5/9r, 5/7r, 1r, 5/4r]
+    FINAL_DRIVE = 11/41r # 1/3.73
 
     attr_accessor :gear, :ratios, :rear_end, :spinner, :fixed_mass
 
@@ -31,10 +41,12 @@ module DrivingPhysics
       @spinner.alpha(torque + @spinner.rotating_friction(omega))
     end
 
+    def rotating_friction(omega)
+      @spinner.rotating_friction(omega)
+    end
 
-    def resistance_torque(alpha, omega)
-      -1 * @spinner.inertial_torque(alpha) +
-        @spinner.rotating_friction(omega)
+    def implied_torque(alpha)
+      @spinner.implied_torque(alpha)
     end
 
     def mass
@@ -60,12 +72,12 @@ module DrivingPhysics
 
     def axle_omega(crank_rpm)
       raise(Disengaged, "Cannot determine axle omega") if @gear == 0
-      DrivingPhysics.omega(crank_rpm) / self.ratio
+      DrivingPhysics.omega(crank_rpm) * self.ratio
     end
 
     def crank_rpm(axle_omega)
       raise(Disengaged, "Cannot determine crank rpm") if @gear == 0
-      DrivingPhysics.rpm(axle_omega) * self.ratio
+      DrivingPhysics.rpm(axle_omega) / self.ratio
     end
 
     def match_rpms(old_rpm, new_rpm)
