27ea8cfbed55fc83598641eb1c33a2b6c98112bd
Rick Hull <rick.hull@gmail.com>
2022-06-16 10:47:39 -0400

    add CLI module, pause and prompt

---
+++ lib/driving_physics/cli.rb
@@ -0,0 +1,17 @@
+module DrivingPhysics
+  module CLI
+    # returns user input as a string
+    def self.prompt(msg = '')
+      print msg + ' ' unless msg.empty?
+      print '> '
+      $stdin.gets.chomp
+    end
+
+    # press Enter to continue
+    def self.pause(msg = '')
+      puts msg unless msg.empty?
+      puts "     [ Press Enter ]"
+      $stdin.gets
+    end
+  end
+end
