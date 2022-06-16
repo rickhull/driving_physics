3dde1203629601c89f977f7c810c94ddd0e15949
Rick Hull <rick.hull@gmail.com>
2022-06-16 14:14:45 -0400

    use Gearbox#alpha; add comments

--- demo/gearbox.rb
+++ demo/gearbox.rb
@@ -1,28 +1,37 @@
 require 'driving_physics/gearbox'
+require 'driving_physics/cli'
 
 include DrivingPhysics
 
 env = Environment.new
 gearbox = Gearbox.new(env)
+puts env
+puts gearbox
+puts
+puts "Spin up the gearbox with 15 Nm of input torque"
+puts "How fast will it go?"
+CLI.pause
 
-# spin up at 6 rad/s/s
-
-# just a target
-alpha = 6.3
+# rotational kinematics
+alpha = 0.0
 omega = 0.0
 theta = 0.0
 
 torque = 15
-
 duration = 20
 
 (duration * env.hz + 1).times { |i|
+  # just for info, not used in the simulation
   friction = gearbox.spinner.rotating_friction(omega)
-  net_torque = torque + friction
-  alpha = gearbox.spinner.alpha(net_torque)
+  net_torque = torque + gearbox.resistance_torque(alpha, omega)
+
+  # update rotational kinematics
+  # gearbox.alpha incorporates friction and inertia
+  alpha = gearbox.alpha(torque, omega: omega)
   omega += alpha * env.tick
   theta += omega * env.tick
 
+  # periodic output
   if i < 10 or
     (i < 100 and i % 10 == 0) or
     (i < 1000 and i % 100 == 0) or
