f4387e4001032ba34d98c166baaabf09c01a58d3
Rick Hull <rick.hull@gmail.com>
2022-06-16 20:00:33 -0400

    improve #to_s; deprecate old stuff

--- lib/driving_physics/powertrain.rb
+++ lib/driving_physics/powertrain.rb
@@ -11,24 +11,24 @@ module DrivingPhysics
     end
 
     def to_s
-      [@motor, @gearbox].join("\n")
+      ["\t[MOTOR]", @motor, "\t[GEARBOX]", @gearbox].join("\n")
     end
 
     def select_gear(gear)
       @gearbox.gear = gear
     end
 
-    def output(rpm, crank_a: 0, crank_o: 0, axle_a: 0, axle_o: 0)
-      [self.axle_torque(rpm, crank_a: crank_a, crank_o: crank_o,
-                        axle_a: axle_a, axle_o: axle_o),
-       self.axle_omega(rpm)]
-    end
+#    def output(rpm, crank_a: 0, crank_o: 0, axle_a: 0, axle_o: 0)
+#      [self.axle_torque(rpm, crank_a: crank_a, crank_o: crank_o,
+#                        axle_a: axle_a, axle_o: axle_o),
+#       self.axle_omega(rpm)]
+#    end
 
     # convert rpm to axle torque, taking motor and gearbox losses into account
-    def axle_torque(rpm, crank_a: 0, crank_o: 0, axle_a: 0, axle_o: 0)
-      @motor.net_torque(rpm, alpha: crank_a) * @gearbox.ratio +
-        @gearbox.resistance_torque(axle_a, axle_o)
-    end
+#    def axle_torque(rpm, crank_a: 0, crank_o: 0, axle_a: 0, axle_o: 0)
+#      @motor.net_torque(rpm, alpha: crank_a) * @gearbox.ratio +
+#        @gearbox.resistance_torque(axle_a, axle_o)
+#    end
 
     def axle_omega(rpm)
       @gearbox.axle_omega(rpm)
