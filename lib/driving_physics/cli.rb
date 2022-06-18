87dee41be4f49b3e7307ca7032867261c0209027
Rick Hull <rick.hull@gmail.com>
2022-06-18 18:14:52 -0400

    add timer functions

--- lib/driving_physics/cli.rb
+++ lib/driving_physics/cli.rb
@@ -8,10 +8,32 @@ module DrivingPhysics
     end
 
     # press Enter to continue
+    # return the elapsed time
     def self.pause(msg = '')
+      t = self.now
       puts msg unless msg.empty?
       puts '     [ Press Enter ]'
       $stdin.gets
+      self.since(t)
+    end
+
+    if defined? Process::CLOCK_MONOTONIC
+      def self.now
+        Process.clock_gettime Process::CLOCK_MONOTONIC
+      end
+    else
+      def self.now
+        Time.now
+      end
+    end
+
+    def self.since(t)
+      self.now - t
+    end
+
+    def self.elapsed(&work)
+      t = self.now
+      return yield, self.since(t)
     end
   end
 end
