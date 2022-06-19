72efe12028fc889901ca95fc043971a0453e0f60
Rick Hull <rick.hull@gmail.com>
2022-06-18 21:48:46 -0400

    modernize CLI

--- demo/scalar_force.rb
+++ demo/scalar_force.rb
@@ -5,18 +5,25 @@ require 'driving_physics/cli'
 include DrivingPhysics
 
 env = Environment.new
+puts env
 
 mass = 1000           # kg
 weight = mass * env.g # N
 drive_force = 7000    # N
 duration = 100        # seconds
-num_ticks = duration * env.hz + 1
+
+puts format("Force: %d N  Mass: %d kg for %d seconds",
+            drive_force, mass, duration)
+CLI.pause
 
 spd = 0.0 # m/s
 pos = 0.0 # m
+num_ticks = duration * env.hz + 1
 
 flag = false
 phase = :accelerate
+paused = 0.0
+start = Timer.now
 
 num_ticks.times { |i|
   # TODO: make the resistance force negative
@@ -36,8 +43,10 @@ num_ticks.times { |i|
     puts format("%d  %.3f m/s/s  %.2f m/s  %.1f m",
                 i.to_f / env.hz, acc, spd, pos)
     if flag
-      CLI.pause
+      paused += CLI.pause
       flag = false
     end
   end
 }
+
+puts Timer.summary(start, num_ticks, paused)
--- demo/vector_force.rb
+++ demo/vector_force.rb
@@ -5,18 +5,29 @@ require 'driving_physics/cli'
 include DrivingPhysics
 
 env = Environment.new
+puts env
 
 mass = 1000           # kg
 weight = mass * env.g # N
 drive_force = DrivingPhysics.random_unit_vector * 7000 # N
 duration = 100        # seconds
-num_ticks = duration * env.hz + 1
 
-pos = Vector[0, 0]      # m
-vel = Vector[0, 0]      # m/s
+puts format("Force: %d N  Dir: %s",
+            drive_force.magnitude,
+            DrivingPhysics.compass_dir(drive_force.normalize))
+puts format("Mass: %d kg for %d seconds", mass, duration)
+CLI.pause
+
+acc = Vector[0, 0] # m/s/s
+vel = Vector[0, 0] # m/s
+pos = Vector[0, 0] # m
+
+num_ticks = duration * env.hz + 1
 
 flag = false
 phase = :accelerate
+paused = 0.0
+start = Timer.now
 
 num_ticks.times { |i|
   net_force = drive_force +
@@ -36,8 +47,10 @@ num_ticks.times { |i|
     puts format("%d  %.3f m/s/s  %.2f m/s  %.1f m",
                 i.to_f / env.hz, acc.magnitude, vel.magnitude, pos.magnitude)
     if flag
-      CLI.pause
+      paused = CLI.pause
       flag = false
     end
   end
 }
+
+puts Timer.summary(start, num_ticks, paused)
