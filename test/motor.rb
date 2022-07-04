14f98da20697941aea09511f7a6320e679ac8ab2
Rick Hull <rick.hull@gmail.com>
2022-07-04 11:19:01 -0400

    add TorqueCurve tests

--- test/motor.rb
+++ test/motor.rb
@@ -55,41 +55,118 @@ describe "interpolate" do
   end
 end
 
+# shorthand
+def tc(rpms, torques)
+  TorqueCurve.new(rpms: rpms, torques: torques)
+end
+
 describe TorqueCurve do
+  before do
+    @default = TorqueCurve.new
+    @rpms =    [500, 1000, 3500, 6000, 7000, 7100]
+    @torques = [  0,  100,  300,  300,  250,    0]
+    @minr = [0, 1]
+    @mint = [0, 0]
+  end
+
   it "maps rpm values to torque values" do
+    expect(@default.torque(3500)).must_be(:>, 100)
   end
 
   describe "initialize" do
     it "accepts two arrays: rpms, and the corresponding torques" do
+      expect(tc(@rpms, @torques)).must_be_kind_of(TorqueCurve)
     end
 
-    it "validates the sizes of rpms and torques match" do
+    it "has defaults for rpms and torques" do
+      expect(@default).must_be_kind_of(TorqueCurve)
     end
+
+    it "validates size match for rpms and torques" do
+      expect(  tc(  @minr, @mint) ).must_be_kind_of TorqueCurve
+      expect { tc(    [0], @mint) }.must_raise
+      expect { tc([0,1,2], @mint) }.must_raise
+      expect { tc(@minr,     [0]) }.must_raise
+      expect { tc(@minr, [0,1,0]) }.must_raise
+
+      rpms = [100, 2000, 30_000]
+      torques = [0, 500, 0]
+
+      expect(  tc( rpms, torques) ).must_be_kind_of TorqueCurve
+      expect { tc( rpms,  [0, 0]) }.must_raise
+      expect { tc([1,2], torques) }.must_raise
+      expect { tc([1,2,3,4], torques) }.must_raise
+    end
+
 
     describe "rpms" do
       it "validates rpms are positive" do
+        expect(  tc(    @minr, @mint) ).must_be_kind_of TorqueCurve
+        expect { tc([-10, 10], @mint) }.must_raise
       end
 
       it "validates rpms are in ascending order" do
+        expect(  tc( @minr, @mint) ).must_be_kind_of TorqueCurve
+        expect { tc([0, 0], @mint) }.must_raise
+        expect(  tc([99, 100], @mint) ).must_be_kind_of TorqueCurve
+        expect { tc([100, 99], @mint) }.must_raise
       end
     end
 
     describe "torques" do
       it "validates that torques start and end at zero" do
+        expect(  tc(@minr, @mint) ).must_be_kind_of TorqueCurve
+        expect { tc(@minr, [1,0]) }.must_raise
+        expect { tc(@minr, [0,1]) }.must_raise
+        expect { tc(@minr, [1,1]) }.must_raise
+
+        expect(  tc([1,2,3], [0,500,0]) ).must_be_kind_of TorqueCurve
+        expect { tc([1,2,3], [1,500,0]) }.must_raise
+        expect { tc([1,2,3], [0,500,1]) }.must_raise
       end
 
       it "validates that torques are positive" do
+        expect(  tc([1,2,3], [0, 5,0]) ).must_be_kind_of TorqueCurve
+        expect { tc([1,2,3], [0,-5,0]) }.must_raise
       end
     end
   end
 
   it "finds the peak torque and corresponding RPM" do
+    rpm, torque = *@default.peak
+    expect(rpm).must_be(:>, 1000)
+    expect(rpm).must_be(:<, 10_000)
+    expect(torque).must_be(:>, 0)
+    expect(torque).must_be(:<, 10_000)
   end
 
   it "interpolates to find the torque for a valid RPM" do
+    tc = tc(@rpms, @torques)
+    r1 = @rpms[1]
+    r2 = @rpms[2]
+
+    expect(tc.torque(r1)).must_equal @torques[1]
+    expect(tc.torque(r2)).must_equal @torques[2]
+
+    r3 = (r1 + r2) / 2
+    t3 = tc.torque(r3)
+    t3_expect = (@torques[1] + @torques[2]) / 2.0
+
+    expect(t3).must_be :>, @torques[1]
+    expect(t3).must_be :<, @torques[2]
+    expect(t3).must_be :>=, t3_expect.floor
+    expect(t3).must_be :<=, t3_expect.ceil
   end
 
   it "tracks min, idle, redline, and max rpms" do
+    expect(@default.min).must_be :>, 0
+    expect(@default.min).must_be :<, 1000
+    expect(@default.idle).must_be :>, @default.min
+    expect(@default.idle).must_be :<, 3000
+    expect(@default.redline).must_be :>, @default.idle
+    expect(@default.redline).must_be :<, 20_000
+    expect(@default.max).must_be :>, @default.redline
+    expect(@default.max).must_be :<, 20_000
   end
 end
 
