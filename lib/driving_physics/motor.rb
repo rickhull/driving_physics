6d115c10886276a37ab238368001f90f1eb63ab9
Rick Hull <rick.hull@gmail.com>
2022-07-04 11:18:43 -0400

    trivial reorder

--- lib/driving_physics/motor.rb
+++ lib/driving_physics/motor.rb
@@ -19,8 +19,8 @@ module DrivingPhysics
   end
 
   class TorqueCurve
-    TORQUES = [  0,   70,  130,  200,  250,  320,  330,  320,  260,    0]
     RPMS    = [500, 1000, 1500, 2000, 2500, 3500, 5000, 6000, 7000, 7100]
+    TORQUES = [  0,   70,  130,  200,  250,  320,  330,  320,  260,    0]
     RPM_IDX = {
       min: 0,
       idle: 1,
