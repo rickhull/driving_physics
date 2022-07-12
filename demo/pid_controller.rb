4f755a0fd0268fc8e5a53a4818d77dce4f840f06
Rick Hull <rick.hull@gmail.com>
2022-07-12 16:16:38 -0400

    simplify for PID stuff

--- demo/pid_controller.rb
+++ demo/pid_controller.rb
@@ -1,26 +1,22 @@
+require 'driving_physics/pid_controller.rb'
 require 'driving_physics/motor'
 require 'driving_physics/cli'
-require 'driving_physics/power'
-require 'driving_physics/pid_controller.rb'
 
 include DrivingPhysics
 
-env = Environment.new
+env = Environment.new { |e| e.hz = 100 }
 motor = Motor.new(env)
 puts env
 puts motor
 puts
 
-puts "* Spin the motor up to #{motor.idle} RPM with the starter motor."
-puts "* Rev it up with the throttle."
-puts "* Let it die."
 CLI.pause
 
 alpha = 0.0
 omega = 0.0
 theta = 0.0
 
-duration = 60
+duration = 3600
 
 status = :ignition
 rpm = 0
@@ -34,12 +30,11 @@ pidc = PIDController.new(motor.idle, dt: env.tick)
 pidc.output_range = (0.0..1.0)
 pidc_error = 0.0
 
-
 num_ticks.times { |i|
   torque = case status
            when :ignition
              motor.starter_torque
-           when :running, :off_throttle, :idling
+           when :running
              motor.torque(rpm)
            else
              0
@@ -51,64 +46,54 @@ num_ticks.times { |i|
 
   net_torque = motor.implied_torque(alpha)
   rpm = DrivingPhysics.rpm(omega)
-  power = DrivingPhysics.power(net_torque, omega)
 
   if status == :ignition
     if rpm > motor.idle
       status = :running
-      flag = true
-      pidc.setpoint = motor.redline
     end
   end
 
   if status == :running
-    if rpm < pidc.setpoint
-      motor.throttle = pidc.update(rpm)
-    else
-      status = :off_throttle
-      motor.throttle = 0.0
-      flag = true
-      pidc.setpoint = motor.idle
-    end
-  end
-
-  if status == :off_throttle
-    if rpm <= 1001
-      status = :idling
-      flag = true
-    end
-  end
-
-  if status == :idling
-    pidc_error += (rpm - pidc.setpoint).abs
     motor.throttle = pidc.update(rpm)
+    pidc_error += pidc.error.abs
   end
 
-  if flag or
-    (i < 10) or
-    (i < 100 and i % 10 == 0) or
-    (i < 1000 and i % 100 == 0) or
-    (i < 10_000 and i % 500 == 0) or
-    (i % 5000 == 0) or
-    (status == :idling and i % 100 == 0)
-    puts Timer.display(ms: i)
-    puts format("Throttle: %.1f%%", motor.throttle * 100)
-    puts format("%d RPM  %.1f Nm (%d Nm)  %.1f kW   Friction: %.1f Nm",
-                DrivingPhysics.rpm(omega),
-                net_torque,
-                torque,
-                power / 1000,
-                motor.spinner.rotating_friction(omega))
+  puts Timer.display(seconds: i.to_f / env.hz)
+  puts format("Throttle: %.1f%%", motor.throttle * 100)
+  puts format("%d RPM  %.1f Nm (%.1f Nm)  Friction: %.1f Nm",
+              DrivingPhysics.rpm(omega),
+              net_torque,
+              torque,
+              motor.spinner.rotating_friction(omega))
+  puts pidc
+  puts "Absolute error %.3f" % pidc_error
+  puts
 
-    if [:idling, :running].include?(status)
-      puts pidc
-      puts "Total error %.3f" % pidc_error if status == :idling
-    end
-    puts
+  error_pct = pidc.error.abs / pidc.setpoint.to_f
 
-    paused += CLI.pause if flag
-    flag = false
+  if error_pct < 0.02 or i % 10 == 0
+    next if status == :ignition and i % 10 != 0
+    loop {
+      puts "Enter > setpoint kp ki kd  " +
+           "Current: #{pidc.setpoint} #{pidc.kp} #{pidc.ki} #{pidc.kd}"
+      print "> "
+      str = $stdin.gets.chomp
+      break if str.empty?
+      parts = str.split(' ')
+      next unless parts.size == 4
+      begin
+        setpoint, kp, ki, kd = *parts.map { |s| s.strip.to_f }
+      rescue e
+        puts e
+        next
+      end
+      p [setpoint, kp, ki, kd]
+      pidc.setpoint = setpoint
+      pidc.kp = kp
+      pidc.ki = ki
+      pidc.kd = kd
+      pidc_error = 0.0
+      break
+    }
   end
 }
-
-puts Timer.summary(start, num_ticks, paused)
