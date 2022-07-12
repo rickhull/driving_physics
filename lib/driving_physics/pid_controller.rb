08c18f061c024c6c289507c446ebfe2aab24773b
Rick Hull <rick.hull@gmail.com>
2022-07-12 16:16:23 -0400

    add Ziegler-Nichols tuning

--- lib/driving_physics/pid_controller.rb
+++ lib/driving_physics/pid_controller.rb
@@ -2,8 +2,8 @@ module DrivingPhysics
   # we will have a control loop
   # SP    - setpoint, this is the desired position
   # PV(t) - process variable, this is the sensed position, varying over time
-  #  e(t  - error, SP - PV
-  # CV(t) (or MV) - manipulated (or control) variable: the controller output
+  #  e(t) - error, SP - PV
+  # CV(t) - control variable: the controller output
 
   # for example, where to set the throttle to maintain 1000 RPM
   # SP - 1000 RPM
@@ -13,6 +13,32 @@ module DrivingPhysics
   class PIDController
     HZ = 1000
     TICK = Rational(1) / HZ
+
+    # Ziegler-Nichols method for tuning PID gain knobs
+    ZN = {
+      :type =>  [:kp, :ti, :td, :ki, :kd],
+      :var  =>  [:ku, :tu, :tu, :ku_over_tu, :ku_tu],
+      'P'  =>   [0.500],
+      'PI' =>   [0.450, 0.800,   nil, 0.540],
+      'PD' =>   [0.800,   nil, 0.125,   nil, 0.100],
+      'PID' =>  [0.600, 0.500, 0.125, 1.200, 0.075],
+      'PIR' =>  [0.700, 0.400, 0.150, 1.750, 0.105],
+      # less overshoot than standard PID below
+      'some' => [0.333, 0.500, 0.333, 0.666, 0.111],
+      'none' => [0.200, 0.500, 0.333, 0.400, 0.066],
+    }
+
+    # ultimate gain, oscillation
+    def self.tune(type, ku, tu)
+      record = ZN[type.downcase] || ZN[type.upcase] || ZN.fetch(type)
+      kp, ti, td, ki, kd = *record
+      kp *= ku if kp
+      ti *= tu if ti
+      td *= tu if td
+      ki *= (ku / tu) if ki
+      kd *= (ku * tu) if kd
+      { kp: kp, ti: ti, td: td, ki: ki, kd: kd }
+    end
 
     attr_accessor :kp, :ki, :kd, :dt, :setpoint, :integral_range, :output_range
     attr_reader :measure, :error, :last_error, :sum_error
