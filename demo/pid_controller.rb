aa47063f392368d98bc0c5007ea69e1e8b1109bb
Rick Hull <rick.hull@gmail.com>
2022-07-16 09:13:23 -0400

    big update

--- demo/pid_controller.rb
+++ demo/pid_controller.rb
@@ -5,20 +5,24 @@ require 'driving_physics/cli'
 include DrivingPhysics
 
 env = Environment.new { |e|
-  e.hz = CLI.prompt("What frequency?", default: 100).to_i
+  e.hz = CLI.prompt("What frequency?", default: 1000).to_i
 }
 motor = Motor.new(env)
 puts env
 puts motor
 puts
 
-# maintain arbitrary RPM
+# maintain arbitrary RPM via throttle adjustment
 pidc = PIDController.new(motor.idle, dt: env.tick) { |p|
-  p.output_range = (0.0..1.0) # throttle
-  p.kp = 0.75
+  p.kp = 0.5
   p.ki = 0.05
-  p.kd = 0.0075
+  p.kd = 0.005
+  p.p_range = (-1.00..1.00)
+  p.i_range = (-0.50..0.50)
+  p.d_range = (-0.25..0.25)
+  p.o_range = ( 0.00..1.00)
 }
+# track the accumulated absolute error
 pidc_error = 0.0
 
 puts pidc
@@ -28,62 +32,92 @@ CLI.pause
 
 alpha = 0.0
 omega = 0.0
-theta = 0.0
 
 duration = 3600
 rpm = 0
 status = :ignition
 
 (duration * env.hz + 1).times { |i|
+  puts Timer.display(seconds: i.to_f / env.hz)
+
+  # GIVEN:
+  # * rpm
+  # DETERMINE:
+  # * torque
+  # * new rpm
+
   case status
   when :ignition
-    status = :running if rpm > motor.idle
     torque = motor.starter_torque
+    puts "IGNITION: Starter torque %d Nm  %d RPM" % [torque, rpm]
+    status = :running if rpm > motor.idle
   when :running
-    motor.throttle = pidc.update(rpm)
-    pidc_error += pidc.error.abs
     torque = motor.torque(rpm)
+    puts format("RUNNING: %.1f Nm @ %d RPM  Throttle: %s",
+                torque, rpm, motor.throttle_pct(3))
+    puts pidc
+    puts "Absolute error %.3f" % pidc_error
   end
 
+  # update kinematics
   alpha = motor.alpha(torque, omega: omega)
   omega += alpha * env.tick
-  theta += omega * env.tick
-
-  net_torque = motor.implied_torque(alpha)
   rpm = DrivingPhysics.rpm(omega)
 
-  puts Timer.display(seconds: i.to_f / env.hz)
-  puts format("Throttle: %.1f%%", motor.throttle * 100)
-  puts format("%d RPM  %.1f Nm (%.1f Nm)  Friction: %.1f Nm",
-              DrivingPhysics.rpm(omega),
-              net_torque,
-              torque,
-              motor.spinner.rotating_friction(omega))
-  puts pidc
-  puts "Absolute error %.3f" % pidc_error
-  puts
+  puts format("Net Torque: %.4f Nm   Friction: %.4f Nm",
+              motor.implied_torque(alpha),
+              motor.friction(omega))
 
-  error_pct = pidc.error.abs / pidc.setpoint.to_f
+  # Finally, update throttle position and RPM
+  # Throttle position is based on PID controller
+  # PID controller looks at current RPM
+  # Update to the new RPM at the very end of the loop
 
-  # prompt every so often
-  if error_pct < 0.02 or i % 10 == 0
-    next if status == :ignition and i % 10 != 0
-    loop {
-      puts
-      str = CLI.prompt("Enter key and value, e.g. > setpoint 3500\n")
-      break if str.empty?
-      parts = str.split(' ')
-      next unless parts.size == 2
-      begin
-        key, val = *parts.map { |s| s.strip.to_f }
-      rescue e
-        puts e
-        next
-      end
-      p [key, val]
-      pidc.send("#{key}=", val)
-      pidc_error = 0.0
-      break
-    }
+  case status
+  when :ignition
+    # ok
+  when :running
+    # new throttle is based on the old (current) rpm
+    motor.throttle = pidc.update(rpm)
+    pidc_error += pidc.error.abs
+    error_pct = pidc.error.abs / pidc.setpoint.to_f
+
+    # prompt every so often
+    if (error_pct < 0.005 or
+        (i < 100 and i % 10 == 0) or
+        (i < 1_000 and i % 100 == 0) or
+        (i < 10_000 and i % 100 == 0))
+      # ask about PID tunables; loop until an acceptable answer
+      loop {
+        # empty answer is perfectly acceptable; exit the loop
+        puts
+        puts format("rpm %.1f\tsetpoint %d\tkp %s\tki %s\tkd %s",
+                    rpm, pidc.setpoint, pidc.kp, pidc.ki, pidc.kd)
+        str = CLI.prompt("Enter key and value, e.g. > setpoint 3500\n")
+        break if str.empty?
+
+        # look for "key value" pairs
+        parts = str.split(' ').map(&:strip)
+        next unless parts.size == 2
+        begin
+          key, val = parts[0].downcase, parts[1].to_f
+        rescue e
+          puts e
+          next
+        end
+
+        # update RPM or PID controller and exit the loop
+        if key == "rpm"
+          rpm = val
+          omega = DrivingPhysics.omega(rpm)
+        else
+          pidc.send("#{key}=", val)
+          pidc_error = 0.0
+        end
+        puts
+        break
+      }
+    end
   end
+  puts
 }
