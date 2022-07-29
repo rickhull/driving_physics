92893dbc90b4b85459de39274f43279c720e4db9
Rick Hull <rick.hull@gmail.com>
2022-07-29 15:07:05 -0400

    modernize

--- demo/pid_controller.rb
+++ demo/pid_controller.rb
@@ -5,22 +5,36 @@ require 'driving_physics/cli'
 include DrivingPhysics
 
 env = Environment.new { |e|
-  e.hz = CLI.prompt("What frequency?", default: 1000).to_i
+  e.hz = CLI.prompt("What frequency? (Hz)", default: 1000).to_i
 }
 motor = Motor.new(env)
 puts env
 puts motor
 puts
 
+
+PID = DeviceControl::PIDController
+models = PID::ZN.keys.join('  ')
+
+puts "PID models: #{models}"
+pid_model = CLI.prompt("What PID model?", default: 'none')
+
+# Ku for 1000 RPM idle has been measured at 8.0
+# Tu is 1000 Hz / 2
+zn = PID.tune(pid_model, 8.0, 1/500r)
+puts format("kp: %.3f\tki: %.1f\tkd: %.8f", zn[:kp], zn[:ki], zn[:kd])
+puts
+
 # maintain arbitrary RPM via throttle adjustment
 pidc = DeviceControl::PIDController.new(motor.idle, dt: env.tick) { |p|
-  p.kp = 3.0
-  p.ki = 0.0
-  p.kd = 0.0
+  p.kp = zn[:kp]
+  p.ki = zn[:ki]
+  p.kd = zn[:kd]
   p.p_range = (-1.00..1.00)
   p.i_range = (-0.50..0.50)
   p.d_range = (-0.25..0.25)
   p.o_range = ( 0.00..1.00)
+  p.e_range = (-1.00..1.00)
 }
 # track the accumulated absolute error
 pidc_error = 0.0
@@ -28,8 +42,7 @@ pidc_error = 0.0
 puts pidc
 puts
 
-throttle_step = CLI.prompt("What throttle step?", default: 0.1).to_f
-smoother = DeviceControl::Smoother.new(max_step: throttle_step)
+CLI.pause
 
 alpha = 0.0
 omega = 0.0
@@ -69,7 +82,7 @@ status = :ignition
               motor.implied_torque(alpha),
               motor.friction(omega))
 
-  # Finally, update throttle position and RPM
+  # Update throttle position and RPM
   # Throttle position is based on PID controller
   # PID controller looks at current RPM
   # Update to the new RPM at the very end of the loop
@@ -77,21 +90,21 @@ status = :ignition
   case status
   when :ignition
     # ok
+    CLI.pause if i % 50 == 0
   when :running
-    motor.throttle = smoother.update(pidc.update(rpm))
+    motor.throttle = pidc.update(rpm)
     pidc_error += pidc.error.abs
     error_pct = pidc.error.abs / pidc.setpoint.to_f
 
     # prompt every so often
-    if (error_pct < 0.005 or
-        (i < 100 and i % 10 == 0) or
+    if (error_pct < 0.0005) or
         (i < 1_000 and i % 100 == 0) or
         (i < 10_000 and i % 100 == 0) or
         (i % 1000 == 0)
       # ask about PID tunables; loop until an acceptable answer
       loop {
         puts
-        puts format("rpm %.1f\tsetpoint %d\tkp %s\tki %s\tkd %s",
+        puts format("rpm %.1f\tsetpoint %d\tkp %.3f\tki %.3f\tkd %.3f",
                     rpm, pidc.setpoint, pidc.kp, pidc.ki, pidc.kd)
         str = CLI.prompt("Enter key and value, e.g. > setpoint 3500\n")
         # empty answer is perfectly acceptable; exit the loop
@@ -102,7 +115,7 @@ status = :ignition
         next unless parts.size == 2
         begin
           key, val = parts[0].downcase, parts[1].to_f
-        rescue e
+        rescue => e
           puts e
           next
         end
@@ -112,7 +125,12 @@ status = :ignition
           rpm = val
           omega = DrivingPhysics.omega(rpm)
         else
-          pidc.send("#{key}=", val)
+          begin
+            pidc.send("#{key}=", val)
+          rescue => e
+            puts e
+            next
+          end
           pidc_error = 0.0
         end
         puts
