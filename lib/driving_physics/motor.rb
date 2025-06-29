45999c71859e2a3e0f69ddfcfa0ddaccd0b222b0
Rick Hull <rwh@arch-intel>
2025-06-29 09:21:32 -0400

    add comments

--- lib/driving_physics/motor.rb
+++ lib/driving_physics/motor.rb
@@ -19,8 +19,11 @@ module DrivingPhysics
   end
 
   class TorqueCurve
+    # at 500 RPM, 0 torque.  1000 RPM = 70 Nm. 7000, redline.  7100, cutoff
     RPMS    = [500, 1000, 1500, 2000, 2500, 3500, 5000, 6000, 7000, 7100]
     TORQUES = [  0,   70,  130,  200,  250,  320,  330,  320,  260,    0]
+
+    # this generates methods below and is not otherwise used
     RPM_IDX = {
       min: 0,
       idle: 1,
